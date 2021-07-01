7fe12110771b86c13a59fb9c1e58ae24adce08d8
Hyelim <hlchoi1223@gmail.com>
2021-07-01 14:01:56 +0900

    [hyelimchoi1223][Edit] 코드 정리

--- week02-210627-210703/hyelimchoi1223/[백준]1744/solved.py
+++ week02-210627-210703/hyelimchoi1223/[백준]1744/solved.py
@@ -1,3 +1,13 @@
+def add_two_elements(iterator):
+    result = 0
+    for i in range(0, len(iterator), 2):
+        if i+1 == len(iterator):
+            result += iterator[i]
+        else:
+            result += (iterator[i]*iterator[i+1])
+    return result
+
+
 N = int(input())
 numbers = []
 for i in range(N):
@@ -5,20 +15,11 @@ for i in range(N):
 
 minus = [x for x in numbers if 0 >= x]
 minus.sort()
-little = [x for x in numbers if 0 < x and x < 2]
+little = [x for x in numbers if x == 1]
 large = [x for x in numbers if x >= 2]
 large.sort(reverse=True)
 
 result = sum(little)
-for i in range(0, len(large), 2):
-    if i+1 == len(large):
-        result += large[i]
-    else:
-        result += (large[i]*large[i+1])
-
-for i in range(0, len(minus), 2):
-    if i+1 == len(minus):
-        result += minus[i]
-    else:
-        result += (minus[i]*minus[i+1])
+result += add_two_elements(large)
+result += add_two_elements(minus)
 print(result)
