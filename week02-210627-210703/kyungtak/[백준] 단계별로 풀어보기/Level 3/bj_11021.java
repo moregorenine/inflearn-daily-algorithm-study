f0c56556a1ff51e217153ecef64e59f63339f85f
박경탁 <qkrrudxkr77@naver.com>
2021-07-13 23:26:25 +0900

    no message

---
+++ week02-210627-210703/kyungtak/[백준] 단계별로 풀어보기/Level 3/bj_11021.java
@@ -0,0 +1,24 @@
+package boj.level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_11021 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int T = Integer.parseInt(st.nextToken());
+
+    for (int i = 1; i <= T; i++) {
+      st = new StringTokenizer(br.readLine());
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
+      int sum = A + B;
+
+      System.out.println("Case #" + i + ": " + sum);
+    }
+  }
+}
